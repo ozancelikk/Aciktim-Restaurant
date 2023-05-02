ad6f5127dc15fe4af69239f0c8d6dcd81d5b9ac3
Ozan <ozancelikk0808@gmail.com>
2023-05-02 17:09:49 +0300

    auth
    auth işlemleri yapıldı

--- src/app/component/auth/login/login.component.ts
+++ src/app/component/auth/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm:FormGroup
+  constructor(private authservice:AuthService,private toastrService:ToastrService,private formBuilder:FormBuilder,private router:Router){}
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+  createLoginForm(){
+    this.loginForm=this.formBuilder.group({
+      email:["",Validators.required],
+      password:["",Validators.required]
+    })
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      let model = Object.assign({},this.loginForm.value);
+      this.authservice.login(model).subscribe(response=>{
+        if (response.success) {
+          localStorage.setItem("restaurantId",response.data.restaurantId);
+          localStorage.setItem("token",response.data.token);
+          this.toastrService.success("Giriş Başarılı","BAŞARILI");
+          setTimeout(() => {
+            this.router.navigate(["/"])
+          }
+            , 500);
+        }
+      },errResponse=>{
+        this.toastrService.error(errResponse.error)
+      })
+    }
+    else{
+      this.toastrService.info("Lütfen Bilgilerinizi Doldurunuz","HATA")
+    }
+  }
 
 }
---
+++ src/app/models/auth/register.ts
@@ -0,0 +1,12 @@
+export interface Register{
+   email:string;
+   password:string;
+   openingTime:string,
+   categoryId:string;
+   closingTime:string;
+   minCartPrice:number;
+   phoneNumber:string;
+   restaurantAddress:string;
+   restaurantName:string;
+   taxNumber:string;
+}
