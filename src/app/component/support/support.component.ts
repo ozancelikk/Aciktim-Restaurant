ca779067301e48f7a79f2f7dbe6af2ea802f46f3
yunusarslans <yunusemrearslan76@hotmail.com>
2023-05-12 15:27:54 +0300

    restaurant-mails

--- src/app/component/support/support.component.ts
+++ src/app/component/support/support.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { RestaurantmailService } from 'src/app/services/restaurant/restaurantmail.service';
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.css']
 })
-export class SupportComponent {
+export class SupportComponent implements OnInit{
+  constructor(private toastrService:ToastrService, private restaurantMailService:RestaurantmailService, private formBuilder:FormBuilder){}
+  form:FormGroup
+  restaurantId:any
+  ngOnInit(): void {
+    this.getRestaurantId()
+    this.createForm()
+  }
+  addMail(){
+    let model = Object.assign({},this.form.value)
+   if(this.form.valid){
+      this.restaurantMailService.addMail(model).subscribe(response=>{
+        response.success? this.toastrService.success("Mesajınız Başarıyla Gönderildi", "BAŞARILI") : this.toastrService.error("Bir Hata Meydana Geldi", "HATA")
+      })
+   }
+   else{
+    this.toastrService.error("Gerekli Alanları Doldurunuz", "HATA")
+   }
+  }
+  createForm(){
+    this.form = this.formBuilder.group({
+      restaurantId:[this.restaurantId,Validators.required],
+      subject:["",Validators.required],
+      content:["",Validators.required],
+      mail:["",Validators.required]
+    })
+  }
+  getRestaurantId(){
+    this.restaurantId = localStorage.getItem("restaurantId")
+
+  }
 
 }
---
+++ src/app/models/restaurant/restaurantMail.ts
@@ -0,0 +1,6 @@
+export interface RestaurantMail{
+    restaurantId:string;
+    mail:string;
+    subject:string;
+    content:string;
+}
